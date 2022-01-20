00940c7b8da27d5c8f8acb6a20e670cd240a5d96
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2022-01-20 11:57:09 -0800

    Installing/uninstalling direnv itself with the plugin

--- conf.d/_wyvernzora_direnv.fish
+++ conf.d/_wyvernzora_direnv.fish
@@ -1,8 +1,15 @@
+fish_add_path $HOME/.bin
 
-function _direnv_on_install --on-event direnv.fish_oninstall
+function _direnv_install --on-event _wyvernzora_direnv_install
     if not type -q direnv
-        echo 'warning: could not find installed direnv'
-    end 
+        echo 'Could not locate direnv, installing'
+        test -d $HOME/.bin || mkdir $HOME/.bin
+        curl -sfL 'https://direnv.net/install.sh' | bin_path=$HOME/.bin bash
+    end
+end
+
+function _direnv_uninstall --on-event _wyvernzora_direnv_uninstall
+    rm $HOME/.bin/direnv
 end
 
 direnv hook fish | source
